5a224b1ee50692e98108152311a84ed9baa81a7f
y-kei-yu <yyouhey617@gmail.com>
2026-01-13 07:56:33 +0900

    もっと見るボタン画面表示（内部処理未実施）

---
+++ src/app/components/layout/Morebutton.tsx
@@ -0,0 +1,5 @@
+export default function Morebutton() {
+    return (
+        <button className="btn justify-end-safe">もっと見る→</button>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
 import { getAllArticles } from "../blogAPI";
 import ArticleList from "./components/ArticleList";
+import Morebutton from "./components/layout/Morebutton";
 
 export default async function Page() {
   const articles = await getAllArticles();
-  //console.log(articles);
+  console.log(articles);
 
   return (
     <>
-      <div className="w-full flex flex-col items-center px-3 my-6">
-        <section className="w-full flex flex-col items-center px-3">
+      <div className="w-full flex flex-col px-3 my-6">
+        <section className="w-full flex flex-col  px-3">
+          <div className="flex justify-between">
+            <h2 className="text-2xl font-bold">個人記事</h2>
+            <Morebutton />
+          </div>
           <ArticleList articles={articles} />
         </section>
       </div>
