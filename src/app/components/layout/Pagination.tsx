5b69c596b160dd0b97754ce27c75b0e8b17ceb48
y-kei-yu <yyouhey617@gmail.com>
2026-01-20 23:54:07 +0900

    ページング機能実装途中

--- src/app/components/layout/Pagination.tsx
+++ src/app/components/layout/Pagination.tsx
@@ -1,4 +1,17 @@
+import { useState } from "react"
+
 export default function Pagination() {
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+
+    /** todo: ページネーションのロジックを実装する
+    前ボタンを押したら currentPage をデクリメント
+    次ボタンを押したら currentPage をインクリメント
+    次ボタンを押したらAPIが呼ばれる
+
+
+    */
     return (
         <div className="w-full flex justify-center my-6">
             <button className="btn">前へ</button>
