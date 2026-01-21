dccf255fdf0e1bf3b94bf161a68d381f43442afe
y-kei-yu <yyouhey617@gmail.com>
2026-01-21 23:42:58 +0900

    ページング機能作成途中②

--- src/app/individuals/page.tsx
+++ src/app/individuals/page.tsx
@@ -2,9 +2,20 @@ import { fetchArticles } from "@/blogAPI";
 import ArticleList from "../components/ArticleList";
 import Pagination from "../components/layout/Pagination";
 
-export default async function Individuals() {
-    const articles = await fetchArticles(8, 1);
-    console.log(articles);
+
+type IndividualsProps = {
+    searchParams?: {
+        page?: string;
+    };
+};
+
+export default async function Individuals({ searchParams }: IndividualsProps) {
+
+    const page = Number(searchParams?.page ?? "1");
+    const safePage = Number.isFinite(page) && page > 0 ? page : 1;
+
+    const articles = await fetchArticles(8, safePage);
+    console.log(articles)
 
     return (
         <div className="w-full flex flex-col px-3 my-6">
