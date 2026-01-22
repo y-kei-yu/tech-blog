9d02c1931b2f91a73c88cff98bc84e5dd0a47c9c
y-kei-yu <yyouhey617@gmail.com>
2026-01-22 23:55:22 +0900

    ページング機能実装途中③

--- src/app/components/layout/Pagination.tsx
+++ src/app/components/layout/Pagination.tsx
@@ -1,17 +1,22 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
 import { useState } from "react"
 
 export default function Pagination() {
 
+    const searchParams = useSearchParams();
+    let pageParam = searchParams.get("page");
+
+    if (!pageParam) {
+        pageParam = "1";
+    } else {
+        pageParam = Number(pageParam).toString();
+    }
+    const initPage = pageParam ? parseInt(pageParam) : 1;
+
     const [currentPage, setCurrentPage] = useState(1);
-    const [totalPages, setTotalPages] = useState(1);
 
-    /** todo: ページネーションのロジックを実装する
-    前ボタンを押したら currentPage をデクリメント
-    次ボタンを押したら currentPage をインクリメント
-    次ボタンを押したらAPIが呼ばれる
-
-
-    */
     return (
         <div className="w-full flex justify-center my-6">
             <button className="btn">前へ</button>
