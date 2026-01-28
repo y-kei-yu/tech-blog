a1abe3d3aac8c99da19a5ef5abf8c3ab7973aca9
y-kei-yu <yyouhey617@gmail.com>
2026-01-28 23:53:35 +0900

    microCMS実装途中

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import { fetchArticles } from "../blogAPI";
+import { fetchArticles, fetchMicroCMSArticles } from "../blogAPI";
 import ArticleList from "./components/ArticleList";
 import Morebutton from "./components/layout/Morebutton";
 
 export default async function Page() {
   const articles = await fetchArticles(4, 1);
+  const microCMSArticles = await fetchMicroCMSArticles(4, 1);
   console.log(articles);
+  console.log(microCMSArticles);
 
   // エラーを意図的に発生させる
   // throw new Error('Test error handling');
@@ -22,7 +24,7 @@ export default async function Page() {
         <section className="w-full flex flex-col  px-3 mt-6">
           <div className="flex justify-between">
             <h2 className="text-2xl font-bold">ブログ記事</h2>
-            <Morebutton href="/individuals" />
+            <Morebutton href="/blogs" />
           </div>
           <ArticleList articles={articles} />
         </section>
--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -4,25 +4,47 @@ export const fetchArticles = async (
   // 1ページに何件データを表示するか
   perPage: number,
   // 何ページ目のデータを取得するか
-  page: number
+  page: number,
 ): Promise<Article[]> => {
   // json-serverから記事データを取得
   //const res = await fetch(`http://localhost:3001/posts`, { cache: "no-store" });
 
   const baseURL = process.env.BASE_URL;
-  console.log(`Fetching articles: perPage=${perPage}, page=${page}`);
+  // console.log(`Fetching articles: perPage=${perPage}, page=${page}`);
 
-  console.log(`ifのあとFetching articles: perPage=${perPage}, page=${page}`);
+  // console.log(`ifのあとFetching articles: perPage=${perPage}, page=${page}`);
 
   // QiitaAPIから記事データを取得
   const res = await fetch(
     `${baseURL}/api/qiita?per_page=${perPage}&page=${page}`,
     {
       cache: "no-store",
-    }
+    },
   );
   if (!res.ok) {
     throw new Error("Failed to fetch articles");
+  }
+  const articles: Article[] = await res.json();
+  return articles;
+};
+
+export const fetchMicroCMSArticles = async (
+  // 1ページに何件データを表示するか
+  perPage: number,
+  // 何ページ目のデータを取得するか
+  page: number,
+): Promise<Article[]> => {
+  const baseURL = process.env.BASE_URL;
+
+  // MicroCMSから記事データを取得
+  const res = await fetch(
+    `${baseURL}/api/cms?per_page=${perPage}&page=${page}`,
+    {
+      cache: "no-store",
+    },
+  );
+  if (!res.ok) {
+    throw new Error("Failed to fetch articles from MicroCMS");
   }
   const articles: Article[] = await res.json();
   return articles;
