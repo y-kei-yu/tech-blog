d212ded5e964a9a6adef3fbf612f5e5d75873726
y-kei-yu <yyouhey617@gmail.com>
2026-02-10 23:07:31 +0900

    もっと見るボタンテスト作成完了

--- src/__tests__/morebutton.spec.tsx
+++ src/__tests__/morebutton.spec.tsx
@@ -1,11 +1,23 @@
 import Morebutton from "@/app/components/layout/Morebutton";
 import { render, screen } from "@testing-library/react";
-import { describe, expect, test } from "vitest";
+import { describe, expect, test, vi } from "vitest";
+
+vi.mock("next/link", () => {
+    return {
+        default: ({ children, href }: { children: React.ReactNode; href: string }) => {
+            return <a href={href}>{children}</a>;
+        }
+    };
+});
 
 describe("MoreButton", () => {
     test("MoreButtonのテキストが表示される", () => {
         render(<Morebutton href="/individuals" />);
         expect(screen.getByText("もっと見る→")).toBeInTheDocument();
-
     });
+    test("リンクが正しく設定されている", () => {
+        render(<Morebutton href="/individuals" />);
+        const link = screen.getByRole("link");
+        expect(link).toHaveAttribute("href", "/individuals");
+    })
 });
