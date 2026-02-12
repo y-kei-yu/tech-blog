ff33b46d856ae157baab21a2865fa1b0cb37fa26
y-kei-yu <yyouhey617@gmail.com>
2026-02-12 23:52:55 +0900

    ページングのテスト完了

--- src/__tests__/pagination.spec.tsx
+++ src/__tests__/pagination.spec.tsx
@@ -1,5 +1,7 @@
+import Pagination from "@/app/components/layout/Pagination";
+import { render, screen } from "@testing-library/react";
 import { describe } from "node:test";
-import { test, vi } from "vitest";
+import { expect, test, vi } from "vitest";
 
 
 vi.mock("next/link", () => {
@@ -10,7 +12,25 @@ vi.mock("next/link", () => {
     };
 });
 describe("Pagination", () => {
-    test("初期状態", () => {
-
+    test("1ページ目では前へがdisabledになり、次ページはLinkになる", () => {
+        render(<Pagination currentPage={1} hasNextPage={true} />);
+        const prev = screen.getByRole("button", { name: "前へ" });
+        expect(prev).toHaveClass("btn-disabled");
+        const link = screen.getByRole("link", { name: "次へ" });
+        expect(link).toHaveAttribute("href", "/individuals?page=2");
+    })
+    test("2ページ目では前へがLinkになり、次ページはdisabledになる", () => {
+        render(<Pagination currentPage={2} hasNextPage={false} />);
+        const prev = screen.getByRole("link", { name: "前へ" });
+        expect(prev).toHaveAttribute("href", "/individuals?page=1");
+        const link = screen.getByRole("button", { name: "次へ" });
+        expect(link).toHaveClass("btn-disabled");
+    })
+    test("3ページ目では前へと次へがLinkになる", () => {
+        render(<Pagination currentPage={3} hasNextPage={true} />);
+        const prev = screen.getByRole("link", { name: "前へ" });
+        expect(prev).toHaveAttribute("href", "/individuals?page=2");
+        const link = screen.getByRole("link", { name: "次へ" });
+        expect(link).toHaveAttribute("href", "/individuals?page=4");
     })
 });
