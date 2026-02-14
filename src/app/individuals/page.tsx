64bd47057871b12b0df0fb1989e14ae7b16547b0
y-kei-yu <yyouhey617@gmail.com>
2026-02-14 23:16:48 +0900

    微調整

--- src/app/individuals/page.tsx
+++ src/app/individuals/page.tsx
@@ -31,7 +31,7 @@ export default async function Individuals({ searchParams }: IndividualsProps) {
         <div className="w-full flex flex-col px-3 my-6">
             <section className="w-full flex flex-col  px-3">
                 <div className="flex justify-between">
-                    <h2 className="text-2xl font-bold">個人記事一覧</h2>
+                    <h2 className="text-2xl font-bold px-3">個人記事一覧</h2>
                 </div>
                 <ArticleList articles={articles} />
             </section>
